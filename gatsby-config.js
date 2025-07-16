871e709bab6d90cc0fb77abc3762da8774fdc0e3
Haley Aaron <haleyaaron@Haleys-MacBook-Pro.local>
2025-07-16 14:00:17 -0700

    Updated the path, metadata, and homepage to map to github pages and read personal information

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Haley Aaron',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Haley Aaron is an engineer and data analyst who specializes in sustainability and climate risk.',
+    siteUrl: 'https://haley-sims.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@Sims_Haley_E',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Haley Aaron',
+        short_name: 'Haley Aaron',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
