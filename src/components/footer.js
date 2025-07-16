90464f6b493b38c4d702d27c94638baae23f6b2f
Haley Aaron <haleyaaron@Haleys-MacBook-Pro.local>
2025-07-16 13:46:39 -0700

    Updated footer to give credit to Brittany Chiang but remove live git stats

--- src/components/footer.js
+++ src/components/footer.js
@@ -68,27 +68,7 @@ const StyledCredit = styled.div`
 `;
 
 const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
-
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
-
+  // Removed githubInfo state and useEffect for GitHub API fetch
   return (
     <StyledFooter>
       <StyledSocialLinks>
@@ -104,23 +84,12 @@ const Footer = () => {
         </ul>
       </StyledSocialLinks>
 
-      <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
-
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
+      <StyledCredit tabIndex="-1">
+        Site design forked from{' '}
+        <a href="https://github.com/bchiang7/v4" target="_blank" rel="noopener noreferrer">
+          Brittany Chiang's open source template
         </a>
+        .
       </StyledCredit>
     </StyledFooter>
   );
