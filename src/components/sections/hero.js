8d177da27ebab20ef902c0c40fe69881b77dbe8c
Haley Aaron <haleyaaron@Haleys-MacBook-Pro.local>
2025-07-15 20:19:43 -0700

    update landing to be relevant to me

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,14 +60,13 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Haley Aaron.</h2>;
+  const three = <h3 className="big-heading">I'm a data nerd.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
+        I’m an engineer interested in organizations and products that support environmentally
+        sustainable infrastructure and responsible decision making{' '}
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
           Upstatement
         </a>
@@ -78,10 +77,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="mailto:haleyesims@gmail.com"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Get in touch!
     </a>
   );
 
